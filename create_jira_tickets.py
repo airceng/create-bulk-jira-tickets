533888dc63c4383cba5f09091e7e556c808372e0
Abheetha Ishan Rangana <d.m.abheethaishan@gmail.com>
2023-09-26 09:54:20 +0530

    Update create_jira_tickets.py

--- create_jira_tickets.py
+++ create_jira_tickets.py
@@ -10,35 +10,68 @@ from jinja2 import Template
 # Jira API URL
 base_url = "https://abheetha-ishan.atlassian.net"
 api_url = f"{base_url}/rest/api/3/issue"
-
-# Load the Jinja2 template
-with open('jira_ticket_template.j2', 'r') as template_file:
-    template_content = template_file.read()
-jinja_template = Template(template_content)
+general_template = "templates/general_jira_ticket_template.j2"
+okra_template = "templates/okra_jira_ticket_template.j2"
 
 # Extract parameters from command-line arguments
 jira_username = sys.argv[1] 
 jira_api_token = sys.argv[2]
-project = sys.argv[3]
-summary = sys.argv[4]
-jira_parent_id = sys.argv[5]
-jira_assignee_id = sys.argv[6]
-goal = sys.argv[7]
-value = sys.argv[8]
-description = sys.argv[9]
-acceptance_criteria = sys.argv[10]
+template_type = sys.argv[3]
 
-# Create the ticket data dictionary
-ticket_data = {
-    "project": project,
-    "summary": summary,
-    "jira_parent_id": jira_parent_id,
-    "jira_assignee_id": jira_assignee_id,
-    "goal": goal,
-    "value": value,
-    "description": description,
-    "acceptance_criteria": acceptance_criteria
-}
+if template_type == "GENERAL":
+    # Load the Jinja2 template
+    with open(general_template, 'r') as template_file:
+        template_content = template_file.read()
+    jinja_template = Template(template_content)
+    
+    # Extract parameters from command-line arguments
+    project = sys.argv[4]
+    summary = sys.argv[5]
+    jira_parent_id = sys.argv[6]
+    jira_assignee_id = sys.argv[7]
+    goal = sys.argv[8]
+    value = sys.argv[9]
+    description = sys.argv[10]
+    acceptance_criteria = sys.argv[11]
+    
+    # Create the ticket data dictionary
+    ticket_data = {
+        "project": project,
+        "summary": summary,
+        "jira_parent_id": jira_parent_id,
+        "jira_assignee_id": jira_assignee_id,
+        "goal": goal,
+        "value": value,
+        "description": description,
+        "acceptance_criteria": acceptance_criteria
+    }
+else:
+    # Load the Jinja2 template
+    with open(okra_template, 'r') as template_file:
+        template_content = template_file.read()
+    jinja_template = Template(template_content)
+    
+    # Extract parameters from command-line arguments
+    project = sys.argv[4]
+    summary = sys.argv[5]
+    jira_parent_id = sys.argv[6]
+    jira_assignee_id = sys.argv[7]
+    goal = sys.argv[8]
+    value = sys.argv[9]
+    #description = sys.argv[10]
+    #acceptance_criteria = sys.argv[11]
+    
+    # Create the ticket data dictionary
+    ticket_data = {
+        "project": project,
+        "summary": summary,
+        "jira_parent_id": jira_parent_id,
+        "jira_assignee_id": jira_assignee_id,
+        "goal": goal,
+        "value": value,
+        #"description": description,
+        #"acceptance_criteria": acceptance_criteria
+    }
 
 # Render the Jinja2 template
 ticket_json = jinja_template.render(ticket_data)
